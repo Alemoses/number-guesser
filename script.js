9ed74d684d36fd82b9a1988443a051620b318d86
Alex <alexnowrang@gmail.com>
2023-08-14 11:35:24 -0500

    Add logic to randomly generate number between 0-9

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+//This function generates a random number that user and computer are competing to guess
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10);
+}
